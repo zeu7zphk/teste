2be05c1790d2a85a5d07b7e15abe37ae119f458a
zeu7zphk <101952923+zeu7zphk@users.noreply.github.com>
2025-05-15 13:49:49 -0300

    Delete quiz-promo-main directory

--- quiz-promo-main/src/app/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '../../../prisma/app/generated/prisma/client';
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-globalForPrisma.prisma = prisma;
